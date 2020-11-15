bd0bd8cf30f2a41643d9300360086f78380b2695
ashley <ashleyhernandez810.ah@gmail.com>
2020-11-14 23:19:44 -0500

    commit tree 27092dae52afc08ecfefbd6a56b4f1bbb7b009c9

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,68 @@
+require 'pry'
+
 class Owner
-  # code goes here
+
+ attr_reader :name, :species
+ @@all = []
+
+ def initialize(name, species = "human")
+  @name = name
+  @species = species
+  @@all << self
+ end
+
+
+ def say_species
+  "I am a #{@species}."
+ end
+
+def cats
+  Cat.all.select {|cats| cats.owner == self}
+end
+
+def dogs
+  Dog.all.select {|dogs| dogs.owner == self}
+end
+
+def buy_cat(cat)
+  Cat.new(cat, self)
+end
+
+def buy_dog(dog)
+  Dog.new(dog, self)
+end 
+
+def walk_dogs
+ self.dogs.collect {|dog| dog.mood = "happy"}
+end 
+
+def feed_cats
+  self.cats.collect {|cat| cat.mood = "happy"}
+end
+
+def sell_pets
+  pets = dogs + cats
+  pets.each do |pet|
+    pet.mood = "nervous"
+    pet.owner = nil
+  end
+end
+
+def list_pets
+  "I have #{dogs.count} dog(s), and #{cats.count} cat(s)."
+end 
+
+
+ def self.all 
+  @@all
+ end
+
+ def self.count
+  @@all.size
+ end
+
+ def self.reset_all
+  @@all.clear
+ end
+  
 end
